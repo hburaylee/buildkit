2e2df04a3928af31f3cbf1e3e6bf30152f6387fd
Ray Lee <hburaylee@gmail.com>
2026-04-15 16:41:46 +0800

    build-keycompute: disable install useless deb packages
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

--- build-keycompute.sh
+++ build-keycompute.sh
@@ -5,20 +5,19 @@ app="keycompute"
 echo ">>> build ${app}"
 
 apt_install() {
-    # apt install -y podman
+#    apt install -y podman
     apt install -y redis
-    apt install -y postgresql
     apt install -y postgresql-contrib
     apt install -y librust-openssl-sys-dev
-    apt install -y librust-glib-sys-dev
-    apt install -y librust-cairo-sys-rs-dev
-    apt install -y libgdk-pixbuf-2.0-dev
-    apt install -y librust-atk-sys-dev
-    apt install -y librust-pango-sys-dev
-    apt install -y librust-gdk-dev
-    apt install -y libsoup-3.0-dev
-    apt install -y libjavascriptcoregtk-4.1-dev
-    apt install -y libwebkit2gtk-4.1-dev
+#    apt install -y librust-glib-sys-dev
+#    apt install -y librust-cairo-sys-rs-dev
+#    apt install -y libgdk-pixbuf-2.0-dev
+#    apt install -y librust-atk-sys-dev
+#    apt install -y librust-pango-sys-dev
+#    apt install -y librust-gdk-dev
+#    apt install -y libsoup-3.0-dev
+#    apt install -y libjavascriptcoregtk-4.1-dev
+#    apt install -y libwebkit2gtk-4.1-dev
 }
 
 setting_podman() {
