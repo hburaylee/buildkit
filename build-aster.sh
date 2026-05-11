74c79ed85388f22a212af4ed48a80228cc2a78d1
Ray Lee <hburaylee@gmail.com>
2026-05-11 13:13:38 +0800

    build-aster: add run_kernel and sync comand
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

--- build-aster.sh
+++ build-aster.sh
@@ -8,10 +8,34 @@ mnt_dir="/workspace"
 
 echo ">>> build ${app}"
 
+sync_github() {
+    git remote add github https://github.com/asterinas/asterinas
+    git fetch github
+    git checkout main       # 或你使用的默认分支 (如 master)
+    git merge github/main   # 合并GitHub 的更改
+    git push origin main    # 推送到 GitLab
+}
+
+do_runkernel() {
+    make ENABLE_KVM=1 run_kernel
+}
+
+if [ -n "$1" ]; then
+    if [ "$1" == "run_kernel" ]; then
+        do_runkernel
+    fi
+    exit 0
+fi
+
 if [ "$ws" != 1 ]; then
     mnt_dir="$PWD/$app"
     [ ! -d ${app} ] && git clone https://cnb.cool/rayylee/asterinas
 fi
+
+pushd ${mnt_dir}
+    sync_github
+popd
+cp -f $0 ${mnt_dir}/
 
 docker_id=$(docker ps -a 2>/dev/null | grep -m 1 "asterinas/asterinas" | awk '{print $1}')
 
@@ -24,9 +48,8 @@ else
     if [ -n "$is_exited" ]; then
         docker start ${docker_id}
     fi
-    docker exec -it ${docker_id} /bin/sh
+    docker exec -it ${docker_id} /bin/bash
 fi
 
-# make ENABLE_KVM=1 run_kenel
 
 exit 0
