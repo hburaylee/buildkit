ba3c18ee17d9750ad5c8b66998142f0a71f9b47f
Ray Lee <hburaylee@gmail.com>
2026-05-29 15:35:01 +0800

    add build-fio.sh
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

---
+++ build-fio.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+app="fio"
+repo="https://github.com/axboe/$app"
+repo_tag="fio-3.42"
+
+echo ">>> build ${app}"
+
+if [ ! -d $app ]; then
+    git clone $repo -b $repo_tag
+fi
+
+if [ -d $app ]; then
+    pushd $app
+    ./configure --build-static
+    make -j$(nproc)
+    popd
+fi
+
+exit 0
