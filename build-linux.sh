09275b8c27dcef9e2cdad392477ae845f13d0c6c
Ray Lee <hburaylee@gmail.com>
2026-06-23 10:39:50 +0800

    bootstrap: modify build-linux for sync upstream
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

--- build-linux.sh
+++ build-linux.sh
@@ -3,20 +3,34 @@
 app="linux"
 repo="https://cnb.cool/rayylee/linux"
 
-ws_dir="${WS:-1}"
 app_dir="/workspace/$app"
 
 echo ">>> build ${app}"
 
-if [ -z "$WS" ]; then
-    app_dir="$PWD/$app"
+
+do_clone() {
     if [ ! -d ${app} ]; then
         git clone --depth 1 $repo
     fi
-else
-    app_dir="${ws_dir}/$app"
+}
+
+do_upstream() {
+    do_clone
+    pushd ${app_dir}
+        git remote add upstream https://github.com/torvalds/linux
+        git fetch --depth=1 upstream master
+        git switch --detach upstream/master
+    popd
+}
+
+if [ -n "$1" ]; then
+    if [ "$1" == "upstream" ]; then
+        do_upstream
+    fi
+    exit 0
 fi
 
+do_clone
 pushd ${app_dir}
     [ ! -e .config ] && make defconfig
     make -j $(nproc) compile_commands.json
