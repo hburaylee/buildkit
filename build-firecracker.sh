fc4774410abadf6818ba638c22bdbe0fd23c09d1
Ray Lee <hburaylee@gmail.com>
2026-07-10 11:05:59 +0800

    build-firecracker: add show to download vmlinux and rootfs
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

--- build-firecracker.sh
+++ build-firecracker.sh
@@ -7,6 +7,58 @@ app_dir="$(pwd)/$app"
 
 echo ">>> build ${app}"
 
+show_vmlinux_rootfs() {
+    ARCH="x86_64"
+
+    cat << EOF
+
+=== Test ===
+
+=> https://s3.amazonaws.com/spec.ccfc.min/
+
+EOF
+
+    curl -s "https://s3.amazonaws.com/spec.ccfc.min/?list-type=2&prefix=firecracker-ci/v1.10/${ARCH}/vmlinux-" \
+        | grep -oP '(?<=<Key>)firecracker-ci/v1\.10/'"${ARCH}"'/vmlinux-[0-9.]+(?=</Key>)' \
+        | sort -V | tail -1
+
+
+    curl -s "https://s3.amazonaws.com/spec.ccfc.min/?list-type=2&prefix=firecracker-ci/v1.10/${ARCH}/ubuntu-" \
+        | grep -oP '(?<=<Key>)firecracker-ci/v1\.10/'"${ARCH}"'/ubuntu-[0-9.]+\.(squashfss|ext4)(?=</Key>)' \
+        | sort -V | tail -1
+
+
+    cat << EOF
+
+# cat vmconfig.json
+{
+  "boot-source": {
+    "kernel_image_path": "\$KERNEL_PATH",
+    "boot_args": "console=ttyS0 reboot=k"
+},
+  "drives": [
+    {
+      "drive_id": "rootfs",
+      "path_on_host": "\$ROOTFS_PATH",
+      "is_root_device": true,
+      "is_read_only": false
+    }
+  ],
+  "machine-config": {
+    "vcpu_count": 2,
+    "mem_size_mib": 1024
+  }
+}
+
+firecracker/build/cargo_target/debug/firecracker \
+    --id anonymous-instance \
+    --api-sock firecracker.sock \
+    --log-path firecracker.log  \
+    --config-file vmconfig.json
+
+EOF
+}
+
 do_clone() {
     if [ ! -d ${app} ]; then
         git clone --depth 1 $repo -b v1.16.1
@@ -14,8 +66,9 @@ do_clone() {
 }
 
 do_clone
-pushd ${app_dir}
+pushd ${app_dir} > /dev/null
     cargo build && file ./build/cargo_target/debug/firecracker
-popd
+    show_vmlinux_rootfs
+popd > /dev/null
 
 exit 0
