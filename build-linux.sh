bf7eb1dad84ef830be67938bc2455842f97995f4
Ray Lee <hburaylee@gmail.com>
2026-09-16 09:45:07 +0800

    build-linux: using beijing git mirror
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
+# 最核心、最权威的仓库是 Linus Torvalds 的树（mainline）
+#  https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+# 官方还维护着 stable 稳定版
+#  https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+
+# 清华大学 - 国内最常用的镜像之一，同步较及时
+#   https://mirrors.tuna.tsinghua.edu.cn/git/linux.git
+# 中国科学技术大学 - 支持断点续传（通过 git fetch）
+#   https://mirrors.ustc.edu.cn/linux.git
+# 华中科技大学 - 提供双栈线路
+#   https://mirrors.hust.edu.cn/git/linux.git
+# 南京大学 - 同样提供帮助文档
+#   https://mirror.nju.edu.cn/git/linux.git
+# 北京镜像站 - 由官方团队维护，针对北京地区优化
+#   See: <https://kernel.googlesource.com/pub/scm/docs/kernel/website/+/refs/tags/v2026-09-02-01/content/news/2020-01-11-beijing-git-mirror.rst>
+#   git://kernel.source.codeaurora.cn/pub/scm/.../linux.git
+
 app="linux"
 repo="https://cnb.cool/rayylee/linux"
+upstream_repo="git://kernel.source.codeaurora.cn/pub/scm/.../linux.git"
 
 app_dir="$app"
 
@@ -17,9 +35,12 @@ do_clone() {
 do_upstream() {
     do_clone
     pushd ${app_dir}
-        git remote add upstream https://github.com/torvalds/linux
-        git fetch --depth=1 upstream master
-        git switch --detach upstream/master
+        git fetch --unshallow
+
+        git remote add upstream ${upstream_repo}
+        git fetch upstream master
+        git checkout master
+        git merge upstream/master
     popd
 }
 
